c574994db608e6180ffa5982f2704c777f57c8ff
Alex Bazlinton <abazlinton@gmail.com>
2016-12-16 14:26:34 +0000

    There's a map baby!

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,10 @@
+var MapWrapper = require('./mapWrapper.js');
+
 window.onload = function () {
 
-  var testH2 = document.getElementById('test-h2');
-  testH2.innerText = "YO YO YO!";
-  
+  var worldMapDiv = document.getElementById('world-map');
+  var mapOptions = {container: worldMapDiv, center: {lat: 51.5, lng: -0.127758}, zoom: 10};
+  var mapWrapper = new MapWrapper(mapOptions);
+
+
 };
---
+++ client/src/mapWrapper.js
@@ -0,0 +1,17 @@
+var MapWrapper = function(options){
+  this.googleMap = new google.maps.Map(options.container, {center: options.center, zoom: options.zoom});
+};
+
+MapWrapper.prototype = {
+
+  addMarker: function(color, coords){
+    var pinColor = color;
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap,
+      icon: pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor)
+    })
+  }
+}
+
+module.exports = MapWrapper;
