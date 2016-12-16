287010c0a2c1beaa92eb20366a6369e4075fe3dd
Alex Bazlinton <abazlinton@gmail.com>
2016-12-16 22:43:07 +0000

    added MapWrapper.getMarkersData and .populateMarkers (untested!)

--- client/src/mapWrapper.js
+++ client/src/mapWrapper.js
@@ -17,10 +17,36 @@ MapWrapper.prototype = {
     console.log(returnValue);
   },
 
-  getMarkers: function(){
-    
+  getMarkersData: function(){
+    var url = "http://localhost:3000/markers"
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function () {
+        if (request.status === 200) {
+            var jsonString = request.responseText;
+            var markersData = JSON.parse(jsonString);
+            this.populateMarkers(markersData);
+        };
+    };
+    request.send();
+  },
 
+  populateMarkers: function(markersData){
+    markersData.forEach( function(markerData){
+      var markerOptions = {
+        position: markerData.position,
+        googleMap: this.googleMap,
+        color: markerData.color,
+        label: markerData.label,
+        returnValue: markerData.returnValue,
+        parentWrapper: this
+      var tempMarker = new Marker(markerOptions);
+      this.addMarker(tempMarker);
+      };
+    };
   }
+
+
 }
 
 module.exports = MapWrapper;
