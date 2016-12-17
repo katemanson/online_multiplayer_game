6d415258f54b410d4f31d999676c115a773b8901
Pavlos <pavloskoz@hotmail.com>
2016-12-17 17:43:43 +0000

    returning player from db

--- server/specs/game_spec.js
+++ server/specs/game_spec.js
@@ -4,26 +4,34 @@ var assert = require('assert');
 
 describe ('Game', function(){
 
-  var testGame = new Game({});
+  var testGame;
+  var testPlayer;
 
-  var testPlayer = new Player({
-    id: 1,
-    name: "Andrina",
-    colour: "#0000ff"
+  beforeEach(function(){
+    testGame = new Game({});
+
+    testPlayer = new Player({
+      id: 1,
+      name: "Andrina",
+      colour: "#0000ff"
+    });
+
   });
 
   it('should start with empty players list', function(){
     assert.deepEqual([], testGame.players);
   });
 
-  it('should be possible to add player', function(){
-    testGame.addPlayer(testPlayer);
-    assert.deepEqual(testPlayer, testGame.players[0]);
+  it('should be possible to add player', function(done){
+    testGame.addPlayerToDb(testPlayer);
+    assert.equal("Andrina", testGame.players[testGame.players.length -1 ].name);
   });
 
-  it('should be possible to save player to database', function(){
-
+  it('should be possible to get player from database', function(){
+      testGame.getPlayersFromDb();
+      assert.equal(1, testGame.players.length );
   });
+
 
 
 });
