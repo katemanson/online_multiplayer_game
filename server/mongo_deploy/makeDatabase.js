e8ca385b6463ac7b2878ea723e1c309b15c40fea
Alex Bazlinton <abazlinton@gmail.com>
2016-12-17 15:27:03 +0000

    throw err for database open failure

--- server/mongo_deploy/makeDatabase.js
+++ server/mongo_deploy/makeDatabase.js
@@ -6,12 +6,15 @@ var fileDataObject;
 fs.readFile('./countryData.json', function(err, data) {
   if (err) {
     throw err;
-  }
+  };
   fileDataString = data;
   fileDataObject = JSON.parse(fileDataString);
 
   var url = 'mongodb://localhost:27017/countries';
   MongoClient.connect(url, function(err, db) {
+    if (err) {
+      throw err;
+    };
     var collection = db.collection('data');
     fileDataObject.forEach(function(countryObject){
       collection.insert(countryObject);
