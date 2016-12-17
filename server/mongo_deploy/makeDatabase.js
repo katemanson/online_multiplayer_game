c84cebe1d5a741ae24a1dfbdec5e6ca4bd4c9018
Alex Bazlinton <abazlinton@gmail.com>
2016-12-17 13:45:53 +0000

    populating database from json file! woop

---
+++ server/mongo_deploy/makeDatabase.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var fileDataString;
+var fileDataObject;
+
+fs.readFile('./countryData.json', function(err, data) {
+  if (err) {
+    throw err;
+  };
+  fileDataString = data;
+  fileDataObject = JSON.parse(fileDataString);
+
+  var url = 'mongodb://localhost:27017/countries';
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('data');
+    fileDataObject.forEach(function(countryObject){
+      collection.insert(countryObject);
+    });
+    db.close();
+  });
+});
