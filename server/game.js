b02f45258728d8a3273c011aab258efd6f0f6904
Pavlos <pavloskoz@hotmail.com>
2016-12-17 20:00:17 +0000

    pushing again

--- server/game.js
+++ server/game.js
@@ -1,12 +1,43 @@
+var MongoClient = require('mongodb').MongoClient;
+
+
+
 var Game = function(options){
   this.players = [];
 };
 
 Game.prototype = {
 
-  addPlayer: function(player){
-    this.players.push(player);
+  addPlayerToDb: function(player){
 
+    var url = 'mongodb://localhost:27017/game';
+    MongoClient.connect(url, function(err, db){
+      // console.log(this);
+      if(err){
+        throw err;
+      };
+      var collection = db.collection('players');
+      collection.insert(player);
+      db.close();
+    });
+    this.getPlayersFromDb();
+  },
+
+  getPlayersFromDb: function(){
+    var url = 'mongodb://localhost:27017/game';
+    MongoClient.connect(url, function(err, db){
+      // console.log(this);
+      if(err){
+        throw err;
+      };
+      var collection = db.collection('players');
+      collection.find().toArray(function(err, docs){
+        this.players = docs;
+        console.log(this.players)
+      }.bind(this));
+      db.close();
+      ;
+    }.bind(this))
   }
 };
 
