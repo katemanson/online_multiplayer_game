6d89465f86920c48a94dd8ecccbe5f4ec7bdfa8c
katemanson <katemanson107@hotmail.com>
2016-12-18 17:40:34 +0000

    refactored marker.js to leave marker info windows closed on window load

--- client/src/marker.js
+++ client/src/marker.js
@@ -2,24 +2,34 @@
 var Marker = function(options){
   this.returnValue =  options.returnValue;
   this.initialize(options);
-  this.addInfoWindow(options);
   this.googleMapMarker.addListener('click', this.markerClicked());
+  this.googleMapMarker.addListener('mouseover', this.openInfoWindow());
+  this.googleMapMarker.addListener('mouseout', this.closeInfoWindow());
   this.parentWrapper = options.parentWrapper;
 };
 
 Marker.prototype = {
   initialize: function(options){
 
+    this.googleMap = options.googleMap;
     this.googleMapMarker = new google.maps.Marker({
       position: options.position,
       map: options.googleMap,
       icon: new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + options.color)
     });
+    this.infoWindow = new google.maps.InfoWindow({content: options.label});
   },
 
-  addInfoWindow: function(options) {
-    var infoWindow = new google.maps.InfoWindow({content: options.label});
-    infoWindow.open(options.googleMap, this.googleMapMarker);
+  openInfoWindow: function(){
+    return function(){
+      this.infoWindow.open(this.googleMap, this.googleMapMarker);
+    }.bind(this);
+  },
+
+  closeInfoWindow: function(){
+    return function(){
+      this.infoWindow.close();
+    }.bind(this);
   },
 
   markerClicked: function(){
