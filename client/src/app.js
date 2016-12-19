89d350f10381029fcdc7af343e8278adccbf2335
Clacson <cookiellacson@gmail.com>
2016-12-19 19:00:44 +0000

    remove satellite map button on map

--- client/src/app.js
+++ client/src/app.js
@@ -5,7 +5,7 @@ var PlayerSetup = require('./playerSetup.js');
 window.onload = function() {
 
   var worldMapDiv = document.getElementById('world-map');
-  var mapOptions = {container: worldMapDiv, center: {lat: 30, lng: 0}, zoom: 2};
+  var mapOptions = {container: worldMapDiv, center: {lat: 30, lng: 0}, zoom: 3};
   var mapWrapper = new MapWrapper(mapOptions);
   mapWrapper.getMarkers();
 
